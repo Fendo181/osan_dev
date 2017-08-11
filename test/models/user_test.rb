bc106f297b3b6a245c75a27e83e076eb1d624d71
Fendo181 <fendo181git@gmail.com>
2017-08-11 23:15:18 +0000

    emailとnameに空文字は入力できないバリデーションとテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,17 @@ class UserTest < ActiveSupport::TestCase
  test "should be valid" do
    assert @user.valid?
  end
+
+
+ test "name should be present" do
+  @user.name = " "
+  #falseが返ってきて欲しい
+  assert_not @user.valid?
+ end
+
+ test "email should be present" do
+   @user.email = "     "
+   assert_not @user.valid?
+ end
+
 end
