d89bbf4737c0f95ef1722f2c9b859edd96711cfe
Fendo181 <fendo181git@gmail.com>
2017-08-12 02:47:51 +0000

    ログインページの雛形を作成した。

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
 
 
-  get 'sessions/new'
-
 # users
+get '/signup',  to: 'users#new'
 resources :users
+# session
+get    '/login',   to: 'sessions#new'
+# post   '/login',   to: 'sessions#create'
+delete '/logout',  to: 'sessions#destroy'
 end
