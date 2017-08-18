a562d1680a6db9b80f6d810262a777745ed51a13
Fendo181 <fendo181git@gmail.com>
2017-08-18 15:46:04 +0000

    sessions_controllerのcreatアクションを実装した。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # emailに該当するユーザをみつける
+    user = User.find_by(email: params[:session][:email].downcase)
+    # ユーザーがデータベースにあり、かつ、認証に成功した
+    if user && user.authenticate(params[:session][:password])
+      # 成功時のアクション
+    else
+      flash[:danger] = 'emailもしくはpasswordが正しくありません'
+      render 'new'
+ end
+
 end
