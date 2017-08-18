8e5b9756d45e479d3ad1087d8dbbf316166164c7
endu <futoshi.endo@pepabo.com>
2017-08-18 13:38:22 +0900

    emailに一意性を追加したマイグレーションファイルを作成して実行した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+     # アカウント作成失敗時
+     render 'new'
+    end
+  end
+
   #一旦はViewファイルの為
   def profile
     @user = User.new
   end
 
+  private
+
+   def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+   end
 end
