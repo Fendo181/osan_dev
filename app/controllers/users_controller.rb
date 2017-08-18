0a88cebee11245dc72b3425d5c12ecc3fd7778b0
endu <futoshi.endo@pepabo.com>
2017-08-18 14:00:04 +0900

    ユーザ登録成功時にユーザのshowページへ飛ばすようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+     redirect_to @user
     else
      # アカウント作成失敗時
      render 'new'
