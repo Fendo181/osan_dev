2c83bdc75194d67a8232df97fd496bbc26c46e49
F_endou181 <fendo181git@gmail.com>
2017-08-19 09:54:12 +0900

    エラーメッセージで日本語を表示するようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-     flash[:success] = "アカウント登録成功しました!"
+     flash[:success] = "アカウント登録に成功しました!"
      redirect_to @user
     else
      render 'new'
