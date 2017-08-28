c8fdf21c7f5eaf752744061be1a26f284aec1426
endu <futoshi.endo@pepabo.com>
2017-08-29 07:22:33 +0900

    余計なコメントを外した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+# static_pages
 root 'static_pages#home'
 get '/welcome', to: 'static_pages#welcome'
+
 # users
 get '/signup',  to: 'users#new'
 post '/signup',  to: 'users#create'
@@ -11,20 +13,8 @@ get    '/login',   to: 'sessions#new'
 post   '/login',   to: 'sessions#create'
 delete '/logout',  to: 'sessions#destroy'
 
-# profile
-# get '/profile',  to: 'users#profile'
-# get 'profiles/show'
-# get 'profiles/edit'
-# post 'profiles/update', to: 'profiles#update'
-# get 'profiles/delete'
-
 # communities
 get '/communities', to: 'communities#index'
-# get 'communities/new'
-# get 'communities/show'
-# get 'communities/edit'
-# get 'communities/updtae'
-# get 'communities/delete'
 
 resources :communities
 resources :profiles
