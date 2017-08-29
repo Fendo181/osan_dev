01e2e4d4fc6b8de95916544a827ffcedc15ad922
endu <futoshi.endo@pepabo.com>
2017-08-29 14:03:51 +0900

    一旦、Communityモデルのみで実装するのでリレーション関係をコメントアウト。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819074454) do
+ActiveRecord::Schema.define(version: 20170829031057) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_communities", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "community_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.string "caregory"
+    t.boolean "admin_allowed", default: false, null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_communities_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "gallery_name"
