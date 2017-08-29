b67aa69594106384d80a4b252d83e1de050fbbcd
endu <futoshi.endo@pepabo.com>
2017-08-30 02:11:50 +0900

    いらないmodelを消した。

--- app/models/category_community.rb
+++
@@ -1,4 +0,0 @@
-class CategoryCommunity < ApplicationRecord
-  belongs_to :category
-  belongs_to :community
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829160313) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "category_communities", force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "community_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170829094321) do
 
   create_table "communities", force: :cascade do |t|
     t.string "name"
@@ -64,16 +51,6 @@ ActiveRecord::Schema.define(version: 20170829160313) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_profiles_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "community_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["community_id"], name: "index_relationships_on_community_id"
-    t.index ["user_id", nil], name: "index_relationships_on_user_id_and_fcommunity_id", unique: true
-    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
