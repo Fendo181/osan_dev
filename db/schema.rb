1763770c1cb9fa14901e55740d68cc8ee69fc07b
endu <futoshi.endo@pepabo.com>
2017-08-30 01:04:59 +0900

    relationshipモデルを作成した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829094321) do
+ActiveRecord::Schema.define(version: 20170829160313) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -64,6 +64,16 @@ ActiveRecord::Schema.define(version: 20170829094321) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_profiles_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "community_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["community_id"], name: "index_relationships_on_community_id"
+    t.index ["user_id", nil], name: "index_relationships_on_user_id_and_fcommunity_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
