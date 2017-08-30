9757a56b83df8936a74cf0b854b56eacecc91a25
endu <futoshi.endo@pepabo.com>
2017-08-30 14:32:54 +0900

    seedでcommunityの練習デ−たを上げれるようにした。

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,18 @@ User.create!(name:  "Endu Futoshi",
             )
 Profile.create!(user_id: User.find(2).id)
 
-
-
-# many samle Community
-20.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
+10.times do |n|
+  name = %w(minne作家さん集まれ！ デザフェス出店する人～！ ハンドメイドピアスを作りたい人集まれ！ 〇〇さんの作品ファン アクセサリーの交換会をしたい！！ ゆめかわな作品好きさん ハンドメイド初心者のコミュニティー).sample
+  category = %w(すべて アクセサリー ファッション 雑貨 素材).sample
+  admin_allowed = [true,false].sample
+  content = Faker::Lorem.sentence
+  # user_id = "#{n+1}"
+  picture =
   Community.create!(
-                     name:  name,
+                     name: name,
+                     category: category,
                      content: content,
-                     category:category,
-                     admin_allowed: password,
-                     user_id:user_id
+                     admin_allowed: admin_allowed,
+                     user_id:1
                      )
 end
