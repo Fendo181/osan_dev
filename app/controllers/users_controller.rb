a8c5da2cbf7655a44b8a89de225f7ca0bb8c28a9
endu <futoshi.endo@pepabo.com>
2017-09-08 19:16:26 +0900

    余計なコメントを削除した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #ユーザ登録が成功した時点でProfileも作成する
       @profile = Profile.create(user_id:@user.id)
+      
       log_in @user
-      # flash[:success] = "アカウント登録に成功しました!"
-      #redirect_to @user
       redirect_to '/welcome'
     else
       render 'new'
